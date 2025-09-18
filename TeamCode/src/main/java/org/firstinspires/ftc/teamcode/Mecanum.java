ffb02b504d10181292f469376b90a5dc96a877f0
B-Ricey763 <ultranoob533@gmail.com>
2025-09-18 16:40:43 -0400

    pretty much finished, need noah to help finalize it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -1,8 +1,9 @@
 
 package org.firstinspires.ftc.teamcode;
 
-
 import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Pose2d;
@@ -31,9 +32,11 @@ import com.acmerobotics.roadrunner.ftc.PositionVelocityPair;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.messages.DriveCommandMessage;
+import org.firstinspires.ftc.teamcode.messages.MecanumCommandMessage;
 import org.firstinspires.ftc.teamcode.messages.PoseMessage;
 
 import java.lang.Math;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -42,16 +45,18 @@ public final class Mecanum
 {
     public static class params
     {
-        public static RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        public static RevHubOrientationOnRobot.UsbFacingDirection usbFacingDirection = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        public RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        public RevHubOrientationOnRobot.UsbFacingDirection usbFacingDirection = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
-        public static double tickPerRev = 8192, wheelID = 38, gearRatio = 1;
-        public static double ticksToInch = (tickPerRev/(wheelID * Math.PI) * 0.041);
-        public static double lateralInPerTick = ticksToInch;
-        public static double trackWidthTicks = 0;
+        public double kS, kV, kA = 0;
 
-        public static double speed, velocity, acceleration = 0;
-        public static double maxWheelVel = 50, minProfileAccel = -30, maxProfileAccel = 50;
+        public double tickPerRev = 8192, wheelID = 38, gearRatio = 1;
+        public double ticksToInch = (tickPerRev/(wheelID * Math.PI) * 0.041);
+        public double lateralInPerTick = ticksToInch;
+        public double trackWidthTicks = 0;
+
+        public double speed, velocity, acceleration = 0;
+        public double maxWheelVel = 50, minProfileAccel = -30, maxProfileAccel = 50;
         public double maxAngVel = Math.PI, maxAngAccel = Math.PI;
 
         public double axialGain , lateralGain, headingGain = 0.0;
@@ -91,6 +96,15 @@ public final class Mecanum
     params.ticksToInch / params.lateralInPerTick
     );
 
+    public final TurnConstraints defaultTurnConstraints = new TurnConstraints(
+            params.maxAngVel, -params.maxAngAccel, params.maxAngAccel);
+    public final VelConstraint defaultVelConstraint =
+            new MinVelConstraint(Arrays.asList(
+                    kinematics.new WheelVelConstraint(params.maxWheelVel),
+                    new AngularVelConstraint(params.maxAngVel)
+            ));
+    public final AccelConstraint defaultAccelConstraint =
+            new ProfileAccelConstraint(params.minProfileAccel, params.maxProfileAccel);
 
     public class DriveLocalize implements Localizer
     {
@@ -276,7 +290,118 @@ public final class Mecanum
 
             driveCommandWriter.write(new DriveCommandMessage(command));
 
+            MecanumKinematics.WheelVelocities<Time> wheelVels = kinematics.inverse(command);
+            double voltage = voltageSensor.getVoltage();
+
+            final MotorFeedforward feedforward = new MotorFeedforward(params.kS, params.kV / params.ticksToInch, params.kA / params.ticksToInch);
+            double frontLeftPower = feedforward.compute(wheelVels.leftFront) / voltage;
+            double frontRightPower = feedforward.compute(wheelVels.rightFront) / voltage;
+            double backLeftPower = feedforward.compute(wheelVels.leftBack) / voltage;
+            double backRightPower = feedforward.compute(wheelVels.rightBack) / voltage;
+            mecanumCommandWriter.write(new MecanumCommandMessage(voltage, frontLeftPower, frontRightPower, backLeftPower, backRightPower));
+
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
+
+            p.put("x", localizer.getPose().position.x);
+            p.put("y", localizer.getPose().position.y);
+            p.put("heading (deg)", Math.toDegrees(localizer.getPose().heading.toDouble()));
+
+            Pose2d error = txWorldTarget.value().minusExp(localizer.getPose());
+            p.put("xError", error.position.x);
+            p.put("yError", error.position.y);
+            p.put("headingError (deg)", Math.toDegrees(error.heading.toDouble()));
+
+            // only draw when active; only one drive action should be active at a time
+            Canvas c = p.fieldOverlay();
+            drawPoseHistory(c);
+
+            c.setStroke("#4CAF50");
+            Drawing.drawRobot(c, txWorldTarget.value());
+
+            c.setStroke("#3F51B5");
+            Drawing.drawRobot(c, localizer.getPose());
+
+            c.setStroke("#4CAF50FF");
+            c.setStrokeWidth(1);
+            c.strokePolyline(xPoints, yPoints);
+
             return true;
+        }
+
+        public final class TurnAction implements Action {
+            private final TimeTurn turn;
+
+            private double beginTs = -1;
+
+            public TurnAction(TimeTurn turn) {
+                this.turn = turn;
+            }
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket p) {
+                double t;
+                if (beginTs < 0) {
+                    beginTs = Actions.now();
+                    t = 0;
+                } else {
+                    t = Actions.now() - beginTs;
+                }
+
+                if (t >= turn.duration) {
+                    frontLeft.setPower(0);
+                    frontRight.setPower(0);
+                    backLeft.setPower(0);
+                    backRight.setPower(0);
+
+                    return false;
+                }
+
+                Pose2dDual<Time> txWorldTarget = turn.get(t);
+                targetPoseWriter.write(new PoseMessage(txWorldTarget.value()));
+
+                PoseVelocity2d robotVelRobot = updatePoseEstimate();
+
+                PoseVelocity2dDual<Time> command = new HolonomicController(
+                        params.axialGain, params.lateralGain, params.headingGain,
+                        params.axialVelGain, params.lateralVelGain, params.headingVelGain
+                )
+                        .compute(txWorldTarget, localizer.getPose(), robotVelRobot);
+                driveCommandWriter.write(new DriveCommandMessage(command));
+
+                MecanumKinematics.WheelVelocities<Time> wheelVels = kinematics.inverse(command);
+                double voltage = voltageSensor.getVoltage();
+                final MotorFeedforward feedforward = new MotorFeedforward(params.kS,
+                        params.kV / params.ticksToInch, params.kA / params.ticksToInch);
+                double leftFrontPower = feedforward.compute(wheelVels.leftFront) / voltage;
+                double leftBackPower = feedforward.compute(wheelVels.leftBack) / voltage;
+                double rightBackPower = feedforward.compute(wheelVels.rightBack) / voltage;
+                double rightFrontPower = feedforward.compute(wheelVels.rightFront) / voltage;
+                mecanumCommandWriter.write(new MecanumCommandMessage(
+                        voltage, leftFrontPower, leftBackPower, rightBackPower, rightFrontPower
+                ));
+
+                frontLeft.setPower(feedforward.compute(wheelVels.leftFront) / voltage);
+                frontRight.setPower(feedforward.compute(wheelVels.rightFront) / voltage);
+                backLeft.setPower(feedforward.compute(wheelVels.leftBack) / voltage);
+                backRight.setPower(feedforward.compute(wheelVels.rightBack) / voltage);
+
+                Canvas c = p.fieldOverlay();
+                drawPoseHistory(c);
+
+                c.setStroke("#4CAF50");
+                Drawing.drawRobot(c, txWorldTarget.value());
+
+                c.setStroke("#3F51B5");
+                Drawing.drawRobot(c, localizer.getPose());
+
+                c.setStroke("#7C4DFFFF");
+                c.fillCircle(turn.beginPose.position.x, turn.beginPose.position.y, 2);
+
+                return true;
+            }
         }
 
         public PoseVelocity2d updatePoseEstimate()
@@ -293,5 +418,50 @@ public final class Mecanum
 
             return vel;
         }
+
+        @Override
+        public void preview(Canvas c)
+        {
+            c.setStroke("#4CAF507A");
+            c.setStrokeWidth(1);
+            c.strokePolyline(xPoints,yPoints);
+        }
+    }
+
+    public PoseVelocity2d updatePoseEstimate()
+    {
+        PoseVelocity2d vel = localizer.update();
+        poseHistory.add(localizer.getPose());
+
+        while(poseHistory.size() > 100)
+        {
+            poseHistory.removeFirst();
+        }
+
+        estimatedPoseWriter.write(new PoseMessage(localizer.getPose()));
+
+        return vel;
+    }
+
+    private void drawPoseHistory(Canvas c) {
+        double[] xPoints = new double[poseHistory.size()];
+        double[] yPoints = new double[poseHistory.size()];
+
+        int i = 0;
+        for (Pose2d t : poseHistory) {
+            xPoints[i] = t.position.x;
+            yPoints[i] = t.position.y;
+
+            i++;
+        }
+
+        c.setStrokeWidth(1);
+        c.setStroke("#3F51B5");
+        c.strokePolyline(xPoints, yPoints);
+    }
+
+    public TrajectoryActionBuilder actionBuilder(Pose2d beginPose)
+    {
+        return new TrajectoryActionBuilder(TurnAction::new, FollowTrajectoryAction::new, new TrajectoryBuilderParams(1e-6, new ProfileParams(0.25, 0.1, 1e-2)), beginPose, 0.0, defaultTurnConstraints, defaultVelConstraint, defaultAccelConstraint);
     }
 }
