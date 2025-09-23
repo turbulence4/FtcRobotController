f1c7b725d6dbf924b0372920a541dfcb0a2ed69b
B-Ricey763 <ultranoob533@gmail.com>
2025-09-23 16:57:40 -0400

    new mecanums
    made a new mecanum based on noah's from last year

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous
+public class Autonomous
+{
+    DcMotor frontLeft, frontRight, backLeft, backRight;
+    ElapsedTime timer = new ElapsedTime();
+
+    //@Override
+    public void runOpMode() throws InterruptedException
+    {
+
+    }
+}
